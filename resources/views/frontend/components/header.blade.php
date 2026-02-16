b9c01c3b84ffe091519c9f15a8713efc6118bfc3
Dewmi Perera <pereradewmi460@gmail.com>
2026-02-16 16:20:00 +0530

    push backend

--- resources/views/frontend/components/header.blade.php
+++ resources/views/frontend/components/header.blade.php
@@ -3,8 +3,8 @@
 
       <a href="index.html" class="logo d-flex align-items-center me-auto">
         <!-- Uncomment the line below if you also wish to use an image logo -->
-        <!-- <img src="assets/img/logo.webp" alt=""> -->
-        <h1 class="sitename">Chroma By BPC</h1>
+        <img src="{{ asset('front-assets/img/logo.png') }}" alt="">
+        {{-- <h1 class="sitename">Chroma By BPC</h1> --}}
       </a>
 
       <nav id="navmenu" class="navmenu">
