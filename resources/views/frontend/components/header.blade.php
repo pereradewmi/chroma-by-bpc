e06d84dc58e385a80eb150d8a9c46d828b695e2f
Dewmi Perera <pereradewmi460@gmail.com>
2026-02-16 19:55:56 +0530

    push front pages UI

--- resources/views/frontend/components/header.blade.php
+++ resources/views/frontend/components/header.blade.php
@@ -1,7 +1,7 @@
   <header id="header" class="header d-flex align-items-center sticky-top">
     <div class="header-container container-fluid container-xl position-relative d-flex align-items-center justify-content-end">
 
-      <a href="index.html" class="logo d-flex align-items-center me-auto">
+      <a href="{{ route('home') }}" class="logo d-flex align-items-center me-auto">
         <!-- Uncomment the line below if you also wish to use an image logo -->
         <img src="{{ asset('front-assets/img/logo.png') }}" alt="">
         {{-- <h1 class="sitename">Chroma By BPC</h1> --}}
@@ -9,10 +9,10 @@
 
       <nav id="navmenu" class="navmenu">
         <ul>
-          <li><a href="index.html" class="active">Home</a></li>
-          <li><a href="students-life.html">Classes</a></li>
+          <li><a href="{{ route('home') }}" class="active">Home</a></li>
+          <li><a href="{{ route('classes') }}">Classes</a></li>
           <li><a href="news.html">Sessions</a></li>
-          <li><a href="events.html">Events</a></li>
+          <li><a href="{{ route('events') }}">Events</a></li>
           <li><a href="contact.html">Contact</a></li>
         </ul>
         <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
